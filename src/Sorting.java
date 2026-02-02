42ad02aad55bbc1aef786b80176275c4f33229fe
KaylaLam <kaylal197@nycstudents.net>
2026-02-02 08:44:28 -0500

    started

---
+++ src/Sorting.java
@@ -0,0 +1,25 @@
+public class Sorting {
+    private String handType;
+    private int count;
+    private String input;
+
+    public Sorting (String input){
+        this.input = input;
+    }
+
+
+    //converts input into an array of integers
+    public int toInt (String input){
+
+    }
+
+    public String getHandType (int count){
+        if (count ==3){
+            handType = "four of a kind";
+        } else if (count == 2){
+            handType = "two pair";
+        } else if (count ==2)
+    }
+
+
+}
