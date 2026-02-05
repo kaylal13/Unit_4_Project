b805b9973263777875d121dce32720368b691323
KaylaLam <kaylal197@nycstudents.net>
2026-02-05 07:59:43 -0500

    updates

---
+++ src/Part1.java
@@ -0,0 +1,16 @@
+public class Part1 {
+    public void splitInput (){
+        String input = file;
+        int bar = input.indexOf("|");
+        String hand = input.substring (0, bar);
+        String bid = input.substring(bar +1);
+        String [] cards = hand.split("");
+    }
+
+
+    int[] handCount = new int[13];
+    for (int i=0;i<handCount.length; i++) {
+
+    }
+}
+
