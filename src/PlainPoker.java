a751d0fc86dd98ebffb3972095d9df029da10ad4
KaylaLam <kaylal197@nycstudents.net>
2026-02-13 08:28:40 -0500

    finished

--- src/PlainPoker.java
+++ src/PlainPoker.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-
 public class PlainPoker {
     private int fiveOfKind;
     private int fourOfKind;
@@ -11,31 +10,24 @@ public class PlainPoker {
     private int twoPair;
     private int onePair;
     private int high;
-
-
     private String[] hands = new String[1000];
     private int[] bids = new int[1000];
     private int[] ranks = new int[1000];
     private int handCount = 0;
 
-
     public void readFile() throws FileNotFoundException {
         File f = new File("src/data");
         Scanner input = new Scanner(f);
 
-
         while (input.hasNextLine()) {
             String line = input.nextLine();
-
 
             int bar = line.indexOf("|");
             String hand = line.substring(0, bar);
             int bidValue = Integer.parseInt(line.substring(bar + 1));
 
-
             hands[handCount] = hand;
             bids[handCount] = bidValue;
-
 
             int type = getType(hand);
             if (type == 7) fiveOfKind++;
@@ -45,14 +37,9 @@ public class PlainPoker {
             else if (type == 3) twoPair++;
             else if (type == 2) onePair++;
             else high++;
-
-
             handCount++;
         }
     }
-
-
-
 
     public int cardValue(String card) {
         if (card.equals("Ace")) return 14;
@@ -62,7 +49,6 @@ public class PlainPoker {
         return Integer.parseInt(card);
     }
 
-
     public int cardValueWild(String card) {
         if (card.equals("Ace")) return 14;
         if (card.equals("King")) return 13;
@@ -71,14 +57,10 @@ public class PlainPoker {
         return Integer.parseInt(card);
     }
 
-
-
-
     public int getType(String hand) {
         String[] cards = hand.split(",");
         int[] counts = new int[5];
         boolean[] counted = new boolean[5];
-
 
         for (int i = 0; i < 5; i++) {
             if (!counted[i]) {
@@ -93,12 +75,10 @@ public class PlainPoker {
             }
         }
 
-
         int pairs = 0;
         boolean hasThree = false;
         boolean hasFour = false;
         boolean hasFive = false;
-
 
         for (int c : counts) {
             if (c == 5) hasFive = true;
@@ -106,7 +86,6 @@ public class PlainPoker {
             if (c == 3) hasThree = true;
             if (c == 2) pairs++;
         }
-
 
         if (hasFive) return 7;
         if (hasFour) return 6;
@@ -116,9 +95,6 @@ public class PlainPoker {
         if (pairs == 1) return 2;
         return 1;
     }
-
-
-
 
     private int cardIndex(String card) {
         if (card.equals("2")) return 0;
@@ -136,7 +112,6 @@ public class PlainPoker {
         return -1;
     }
 
-
     public int getTypeWild(String hand) {
         String[] cards = hand.split(",");
         int numJacks = 0;
@@ -145,7 +120,6 @@ public class PlainPoker {
             if (cards[i].equals("Jack")) numJacks++;
             else count[cardIndex(cards[i])]++;
         }
-
 
         int maxCount = 0;
         int secondMax = 0;
@@ -158,7 +132,6 @@ public class PlainPoker {
             }
         }
 
-
         if (maxCount + numJacks >= 5) return 7;
         if (maxCount + numJacks >= 4) return 6;
         if (maxCount + numJacks >= 3 && secondMax >= 2) return 5;
@@ -168,13 +141,10 @@ public class PlainPoker {
         return 1;
     }
 
-
-
     public int compareHands(String hand1, String hand2) {
         int type1 = getType(hand1);
         int type2 = getType(hand2);
         if (type1 != type2) return type1 - type2;
-
 
         String[] c1 = hand1.split(",");
         String[] c2 = hand2.split(",");
@@ -186,12 +156,10 @@ public class PlainPoker {
         return 0;
     }
 
-
     public int compareHandsWild(String hand1, String hand2) {
         int type1 = getTypeWild(hand1);
         int type2 = getTypeWild(hand2);
         if (type1 != type2) return type1 - type2;
-
 
         String[] c1 = hand1.split(",");
         String[] c2 = hand2.split(",");
@@ -203,14 +171,13 @@ public class PlainPoker {
         return 0;
     }
 
-
-
-
     public void getRanks() {
         for (int i = 0; i < handCount; i++) {
             int strongerCount = 0;
             for (int j = 0; j < handCount; j++) {
-                if (i != j && compareHands(hands[i], hands[j]) > 0) strongerCount++;
+                if (i != j && compareHands(hands[i], hands[j]) > 0) {
+                    strongerCount++;
+                }
             }
             ranks[i] = strongerCount + 1;
         }
@@ -224,20 +191,19 @@ public class PlainPoker {
         for (int i = 0; i < handCount; i++) {
             int strongerCount = 0;
             for (int j = 0; j < handCount; j++) {
-                if (i != j && compareHandsWild(hands[i], hands[j]) > 0) strongerCount++;
+                if (i != j && compareHandsWild(hands[i], hands[j]) > 0) {
+                    strongerCount++;
+                }
             }
             ranksWild[i] = strongerCount + 1;
         }
 
-
         int totalBidWild = 0;
-        for (int i = 0; i < handCount; i++) totalBidWild += ranksWild[i] * bids[i];
-
-
+        for (int i = 0; i < handCount; i++) {
+            totalBidWild += ranksWild[i] * bids[i];
+        }
         System.out.println("Total bid value with jacks wild: " + totalBidWild);
     }
-
-
 
 
     public void displayHand() {
@@ -251,15 +217,11 @@ public class PlainPoker {
     }
 
 
-
-
     public void getTotalBid() {
         int totalBid = 0;
-        for (int i = 0; i < handCount; i++) totalBid += ranks[i] * bids[i];
+        for (int i = 0; i < handCount; i++) {
+            totalBid += ranks[i] * bids[i];
+        }
         System.out.println("Total bid value: " + totalBid);
     }
-
-
-
-
 }
